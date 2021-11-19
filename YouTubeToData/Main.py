7ad3e61a6958fe8d6ad86e8a138bd66bc9263960
arisa <angelevarela@hotmail.com>
2021-11-19 02:53:48 -0800

    Refactored Main/_Gui

--- YouTubeToData/Main.py
+++ YouTubeToData/Main.py
@@ -1,15 +1,7 @@
 import _Gui
-import _CollectChat
 
 def main():
-    # _Gui.DisplayTheProgram()
-    link_of_youtube_video = 'https://www.youtube.com/watch?v=VupT9JCJaOY'
-    video_id = 'VupT9JCJaOY'
-    streamer_name = 'watson'
-    video_name = 'MythOrTreat'
-    file_name = 'watson:MythOrTreat'
-    _CollectChat.collectChat(link_of_youtube_video, video_id, streamer_name, video_name, file_name)
-    # Test this sync
-    # Test this out to see if branch is new
-if __name__ == '__main__':
-    main()
+    _Gui.display_The_Program() # An object isn't needed for this, so we removed the class portion of it
+    _Gui.gui()
+
+if __name__ == '__main__':main()
--- YouTubeToData/_Gui.py
+++ YouTubeToData/_Gui.py
@@ -1,39 +1,72 @@
 import PySimpleGUI as sg
-import _CollectChat
+from _CollectChat import collectChat
 
-class DisplayTheProgram:
-    def __init__(self):
-        gui_title = 'YouTube comment to data'
-        user_display_one = 'YouTube Link?'
-        user_display_two = 'nameOfStreamer:VideoName'
+window = None
+layout = None
+streamer_video_divider = ':'
 
-        layout = [
-            [sg.Text(user_display_one)],
-            [sg.Input()],
-            [sg.Text(user_display_two)],
-            [sg.Input()],
-            [sg.Button('Submit')]
-        ]
+def display_The_Program():
+    global layout
+    global window
 
-        window = sg.Window(gui_title, layout)
-        self.gui(window)
+    gui_title = 'YouTube comment to data'
+    user_display_one = 'YouTube Link?'
+    user_display_two = 'nameOfStreamer:VideoName'
+    keep_after_submission = True
+
+    layout = [
+        [sg.Text(user_display_one)],
+        [sg.Input(do_not_clear=keep_after_submission)],
+        [sg.Text(user_display_two)],
+        [sg.Input(do_not_clear=keep_after_submission)],
+        [sg.Button('Submit')]]
+
+    window = sg.Window(gui_title, layout)
+
+def gui():
+    try:
+        # Event Loop to process "events" and get the "values" of the inputs
+        while True:
+            event, userInput = window.read()
+
+            if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+                break
+
+            link_of_video = userInput[0]
+            if userInput[0] == '':
+                raise ValueError("First value can't be empty")
+
+            video_id = str(userInput[0]).split('=')[1] # Grabs the id from the link
 
 
-    def gui(self, window):
-        try:
-            event, userInput = window.read() # Pauses right here
+            file_name = userInput[1]  # Grabs nameOfStreamer:VideoName for usage next class
+            if userInput[1] == '':
+                raise ValueError("Second value can't be empty")
 
-            link_of_youtube_video = userInput[0] # Holds the link of the YouTube video
-            video_id = str(userInput[0]).split('=')[1]  # Only grabs the ID :: Needed to collect chat
-            file_name = userInput[1] # Grabs nameOfStreamer:VideoName for usage next class
 
-            streamer_name = str(userInput[1]).split(':')[0]
+            streamer_name = str(userInput[1]).split(streamer_video_divider)[0]
+
+            if not look_for_streamer_videoname_divier(userInput[1]):
+                raise ValueError("You forgot to place {}".format(streamer_video_divider))
+
             video_name = str(userInput[1]).split(':')[1]
+            if(video_name == ''):
+                raise ValueError("Video name can't be empty")
 
-            window.close()
-            # Sending the entire link, just the id, and
-            _CollectChat.collectChat(link_of_youtube_video, video_id, streamer_name, video_name, file_name)
-            window.close()
-        except IndexError:
-            print('Did you forget to place "=" or ":"?')
-            window.close()
+            collectChat(link_of_video, video_id, streamer_name, video_name)
+
+        window.close()
+    except ValueError as error:
+        sg.popup_annoying(repr(error))
+        gui()
+    except IndexError:
+        sg.popup_annoying('You forgot to place "="')
+        gui()
+
+def look_for_streamer_videoname_divier(s):
+    try:
+        s.index(streamer_video_divider)
+        return True
+    except ValueError:
+        return False
+
