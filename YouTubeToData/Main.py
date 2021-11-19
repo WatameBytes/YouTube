d5b80489a2c688220859570b826ae197f79e5874
arisa <angelevarela@hotmail.com>
2021-11-19 01:04:41 -0800

    Merge gang

--- YouTubeToData/Main.py
+++ YouTubeToData/Main.py
@@ -10,6 +10,6 @@ def main():
     file_name = 'watson:MythOrTreat'
     _CollectChat.collectChat(link_of_youtube_video, video_id, streamer_name, video_name, file_name)
     # Test this sync
-
+    # Test this out to see if branch is new
 if __name__ == '__main__':
     main()
